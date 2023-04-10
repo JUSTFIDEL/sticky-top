63f2469351a70198aeaeecb9aa2cf7a4ee382171
JUSTFIDEL <fidenki1429@gmail.com>
2023-04-10 14:12:22 +0100

    Navbar Implementation

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const menuToggler = document.querySelector('[data-nav-toggler]')
 const navbar = document.querySelector('[data-nav]')
+const menuTogglerX = document.querySelector('[data-navx-toggler]')
 
 menuToggler.addEventListener('click', () => {
+	navbar.classList.toggle('active')
+})
+
+menuTogglerX.addEventListener('click', () => {
 	navbar.classList.toggle('active')
 })
